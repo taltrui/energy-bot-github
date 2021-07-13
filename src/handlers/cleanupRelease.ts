550e9c1e84b284165566d38595d2ec914ef0d986
Tomás Altrui <tomas.altrui@widergy.com>
2021-07-12 21:18:05 -0300

    errors

--- src/handlers/cleanupRelease.ts
+++ src/handlers/cleanupRelease.ts
@@ -9,19 +9,35 @@ const cleanupRelease = async (context: Context<WebhookPayloadWithRepository>): P
     return;
   }
 
-  const prs = await context.octokit.pulls.list(context.repo());
+  try {
+    const prs = await context.octokit.pulls.list(context.repo());
 
-  const changeBasePromises = prs.data.map((pr) => {
-    if (pr.base.ref === closedPR.head.ref) {
-      return context.octokit.pulls.update(context.repo({ base: 'master', pull_number: pr.number }));
-    }
+    const changeBasePromises = prs.data.map((pr) => {
+      if (pr.base.ref === closedPR.head.ref) {
+        return context.octokit.pulls.update(context.repo({ base: 'master', pull_number: pr.number }));
+      }
 
-    return new Promise((res) => res(null));
-  });
+      return new Promise((res) => res(null));
+    });
 
-  await Promise.all(changeBasePromises);
+    await Promise.all(changeBasePromises);
+  } catch (e) {
+    context.log.fatal(
+      e,
+      `There was an error trying to remove release ${closedPR.head.ref} references in opened pull requests. Aborting.`
+    );
 
-  context.octokit.git.deleteRef(context.repo({ ref: `heads/${closedPR?.head.ref}` }));
+    return;
+  }
+
+  try {
+    context.octokit.git.deleteRef(context.repo({ ref: `heads/${closedPR?.head.ref}` }));
+  } catch (e) {
+    context.log.warn(
+      e,
+      `Unable to delete release ${closedPR.head.ref} branch. It is possible to move forward.`
+    );
+  }
 
   const formattedDate = DateTime.now().toFormat('dd-mm-yyyy');
   const newReleaseBranch = `release-${formattedDate}`;
@@ -35,15 +51,26 @@ const cleanupRelease = async (context: Context<WebhookPayloadWithRepository>): P
       context.repo({ commit_sha: newBranchRef.data.object.sha })
     );
 
-    await context.octokit.git.createCommit(
+    const newCommit = await context.octokit.git.createCommit(
       context.repo({ message: 'chore: init release', tree: newBranchCommit.data.tree.sha })
     );
 
+    await context.octokit.git.updateRef(
+      context.repo({
+        ref: `heads/${newReleaseBranch}`,
+        sha: newCommit.data.sha,
+      })
+    );
+  } catch (e) {
+    context.log.fatal(e, 'There was an error creating the new release branch. Aborting.');
+  }
+
+  try {
     context.octokit.pulls.create(
       context.repo({ base: 'master', head: newReleaseBranch, title: `Release ${formattedDate}` })
     );
   } catch (e) {
-    context.log.fatal(e);
+    context.log.warn(e, "Can't create pull request for new release.");
   }
 };
 
